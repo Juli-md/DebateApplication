f08ea9833e9b9a93d9c1a44aa82fa610023e9586
MedvedevaJulia <yjannalynnm@gmail.com>
2019-03-05 00:32:11 +1000

    Create HomeLayout.js

---
+++ DebateApplicationReact/ClientApp/src/components/HomeLayout.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
+import { MenuExampleSecondary } from './components/MenuExampleSecondary';
+
+
+
+
+export class HomeLayout extends Component {
+    static displayName = HomeLayout.name;
+
+    render() {
+        return (
+            <div>
+                <MenuExampleSecondary />
+
+                <Segment>
+                    {this.props.children}
+                </Segment>
+            </div>
+        );
+    }
+}
